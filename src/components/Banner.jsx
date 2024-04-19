9700f7a0340eb4a307baccb2872644a822607800
jinalsavakiya1473 <jinalsavakiya123@gmail.com>
2024-04-19 18:35:56 +0530

    banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,7 +7,7 @@ function Banner(){
                     <PiMicrophoneStage className="text-light fs-1"></PiMicrophoneStage>
                 </div>
                 <p className='fs-4 py-2 text-light'>20 - 22 January, 2019, Buffelo City</p>
-                <h1 className='pb-5 text-light'>UX CONFERENCE 2019</h1>
+                <h1 className='pb-5 text-light'>UX CONFERENCE 2024</h1>
                 <button className="text-light border-0">BUY TICKET</button>
             </div>
         </div>
